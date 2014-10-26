0a320faa66733ff337009ef4a2f79090e3c57252
Cuiteco <edithreyesloya@hotmail.com>
2014-10-25 21:30:30 -0500

    Assigment2  by Edith Reyes Loya
    Contiene las modificaciones solicitadas para cumplir con el Assigment2,
    codificación y comentarios. erl

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,41 @@
 ## Adecuado
 
 ## Write a short comment describing this function
+## This is the Assigment 2 for R programming course
+## Objective: To calculate the inverse of matrix in cache
+## Modify by Edith Reyes Loya
+##
+## The makeCacheMatrix function creates a list of functions to set and get the
+## matrix, and to set and get de inverse of the certanly matrix to be given 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m<-NULL
+    set<-function(y) {
+        x<<-y
+        m<<-NULL
+    }
+    get<-function() x
+    setsolve<-function(solve) m<<-solve
+    getsolve<-function() m
+    list(set=set,get=get,setsolve=setsolve,getsolve=getsolve)   
 }
 
 
 ## Write a short comment describing this function
+## This function computes the inverse of matrix that was returned by
+## the function makeCacheMatrix
+## If the inverse of matrix was already calculated then return the
+## value of the inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m<-x$getsolve()
+    if(!is.null(m)){
+        message("getting cache data")
+        return(m)
+    }
+    data <- x$get()
+    m<-solve(data, ...)
+    x$setsolve(m)
+    m
 }
